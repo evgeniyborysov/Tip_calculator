302e2a126ef1ed54a8809ab104b3614b7a0766ed
evgeniy.borysov <evgeniy.borysov@mail.com>
2021-11-05 20:19:24 +0200

    second commit

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const totalResult = document.getElementById("total");
 const tipButtons = document.querySelectorAll(".btn-tip");
 const btnReset = document.querySelector(".reset");
 const customInput = document.querySelector(".custom");
+const checkError = document.querySelector(".people__error");
 let percentOfTips;
-
-// console.log(numberOfPeopleInput);
 
 function calculateBill() {
     let bill = +billInput.value;
@@ -16,8 +15,6 @@ function calculateBill() {
     let amountPerPerson = bill / people;
     let tips = (bill * percentOfTips) / people;
     let totalBill = amountPerPerson + tips;
-    // console.log(people);
-    // console.log(percentOfTips);
 
     if (percentOfTips === undefined && people > 0) {
         totalResult.innerHTML = "$" + amountPerPerson.toFixed(2);
@@ -35,28 +32,23 @@ function tipPercentage(e) {
     e.preventDefault();
     percentOfTips = e.target.value;
     percentOfTips = percentOfTips / 100;
-    // console.log(percentOfTips);
     calculateBill();
 }
 
 function error() {
     let people = +numberOfPeopleInput.value;
-    // console.log(`${people} + error`);
     if (people <= 0) {
-        console.log(`${people} + if`);
         if (
-            document
-                .querySelector(".people__error")
-                .classList.contains("hide") &&
+            checkError.classList.contains("hide") &&
             !numberOfPeopleInput.classList.contains("error")
         ) {
-            document.querySelector(".people__error").classList.remove("hide");
+            checkError.classList.remove("hide");
             numberOfPeopleInput.classList.add("error");
             console.log("Ok");
         }
     } else {
         console.log(`${people} + else`);
-        document.querySelector(".people__error").classList.add("hide");
+        checkError.classList.add("hide");
         numberOfPeopleInput.classList.remove("error");
     }
 }
@@ -68,12 +60,13 @@ function reset() {
     customInput.value = "";
     totalResult.innerHTML = "$0.00";
     tipResult.innerHTML = "$0.00";
-    // addActive();
+    for (let i = 0; i < tipButtons.length; i++) {
+        tipButtons[i].classList.remove("btn-active");
+    }
 }
 
 function addActive(e) {
     console.log(e.target);
-    // e.target.classList.add("btn-active");
     for (let i = 0; i < tipButtons.length; i++) {
         if (tipButtons[i] == e.target) {
             e.target.classList.toggle("btn-active");
